3c144b3de54ff16623ceaf79e94b805d3b3adfec
Bunyod Aminov <aminov.bun@gmail.com>
2026-02-01 02:15:07 +0500

    final

--- index.js
+++ index.js
@@ -3,32 +3,38 @@ const app = express();
 
 function gcd(a, b) {
   while (b !== 0n) {
-    [a, b] = [b, a % b];
+    const t = a % b;
+    a = b;
+    b = t;
   }
   return a;
-}
-
-function lcm(a, b) {
-  return (a * b) / gcd(a, b);
 }
 
 app.get('/aminov_bun_gmail_com', (req, res) => {
   const xRaw = req.query.x;
   const yRaw = req.query.y;
 
-  if (typeof xRaw !== 'string' || typeof yRaw !== 'string' || !/^\{[1-9]\d*\}$/.test(xRaw) || !/^\{[1-9]\d*\}$/.test(yRaw)) {
-    res.send('NaN');
+  // только строки вида {123}
+  if (
+    typeof xRaw !== 'string' ||
+    typeof yRaw !== 'string' ||
+    !/^\{[1-9]\d*\}$/.test(xRaw) ||
+    !/^\{[1-9]\d*\}$/.test(yRaw)
+  ) {
+    res.status(200);
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('NaN');
     return;
   }
 
-  try {
-    const x = BigInt(xRaw.slice(1, -1));
-    const y = BigInt(yRaw.slice(1, -1));
+  const x = BigInt(xRaw.slice(1, -1));
+  const y = BigInt(yRaw.slice(1, -1));
 
-    res.send(lcm(x, y).toString());
-  } catch {
-    res.send('NaN');
-  }
+  const result = (x * y) / gcd(x, y);
+
+  res.status(200);
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(result.toString());
 });
 
 app.listen(process.env.PORT || 3000);
