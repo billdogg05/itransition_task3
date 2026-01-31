210f10a1c0ba46c8338678ad1f4308d19ce1cd5a
Bunyod Aminov <aminov.bun@gmail.com>
2026-01-31 16:55:43 +0500

    error fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 function gcd(a, b) {
-  while (b !== 0) {
+  while (b !== 0n) {
     [a, b] = [b, a % b];
   }
   return a;
@@ -16,15 +16,19 @@ app.get('/aminov_bun_gmail_com', (req, res) => {
   const xRaw = req.query.x;
   const yRaw = req.query.y;
 
-  if (!/^\{\d+\}$/.test(xRaw) || !/^\{\d+\}$/.test(yRaw)) {
+  if (typeof xRaw !== 'string' || typeof yRaw !== 'string' || !/^\{[1-9]\d*\}$/.test(xRaw) || !/^\{[1-9]\d*\}$/.test(yRaw)) {
     res.send('NaN');
     return;
   }
 
-  const x = Number(xRaw.slice(1, -1));
-  const y = Number(yRaw.slice(1, -1));
+  try {
+    const x = BigInt(xRaw.slice(1, -1));
+    const y = BigInt(yRaw.slice(1, -1));
 
-  res.send(String(lcm(x, y)));
+    res.send(lcm(x, y).toString());
+  } catch {
+    res.send('NaN');
+  }
 });
 
 app.listen(process.env.PORT || 3000);
